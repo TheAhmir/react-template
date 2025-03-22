e5f98a78828fc45c47ce8901235d5d295288db69
Ahmir Postell <postell.ahmir@gmail.com>
2025-03-22 11:30:58 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +14,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1 className='ahmad'>Hi my names Ahmad</h1>
+          <p>check out my repository</p>
         </a>
       </header>
     </div>
